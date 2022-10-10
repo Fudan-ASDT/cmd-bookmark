ff73d5f2192e93acdc6fd5e77f8f4d8619b42be8
skyleaworlder <870033938@qq.com>
2022-10-10 02:05:11 +0000

    update: ignore grammar

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['/src/grammar/'],
   preset: 'ts-jest',
   testEnvironment: 'node',
   testRegex: '(/test/.*|(\\.|/)(test|spec))\\.tsx?$',
